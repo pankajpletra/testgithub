76adfaac781188397dc20ef364e3a3f6ff60959a
pankajpletra <80086958+pankajpletra@users.noreply.github.com>
2021-04-06 20:25:15 +0530

    Create api,hs and server.js

---
+++ api,hs and server.js
@@ -0,0 +1,53 @@
+// Simple Express server setup to serve for local testing/dev API server
+/////////////////////////////////////////////////////////////////////api.js////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
+const DIST_DIR = './dist';
+
+app.use(express.static(DIST_DIR));
+app.use(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
+
+app.get('/api/v1/endpoint', (req, res) => {
+    res.json({ success: true });
+});
+
+app.listen(PORT, () =>
+    console.log(
+        `✅  API Server started: http://${HOST}:${PORT}/api/v1/endpoint`
+    )
+);
+///////////////////////////////////////////////////////////server.js///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Simple Express server setup to serve the build output
+const compression = require('compression');
+const helmet = require('helmet');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(helmet());
+app.use(compression());
+
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3001;
+const DIST_DIR = './dist';
+
+app.use(express.static(DIST_DIR));
+
+app.use('*', (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
+
+app.listen(PORT, () =>
+    console.log(`✅  Server started: http://${HOST}:${PORT}`)
+);
